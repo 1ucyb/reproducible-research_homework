07702ff64bd432e11be5808dc9f7810bb8b3af76
1ucyb <lucy.back@wadham.ox.ac.uk>
2024-11-30 19:39:57 +0000

    Made Brownian Motion sim reproducible

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -1,55 +1,54 @@
-#install.packages("ggplot2")
-#install.packages("gridExtra")
+# Loads in packages
 
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
+# Sets seed to ensure reproducible results
+
+set.seed(7)
+
+# Function to define a random walk
+
+random_walk = function(n_steps){ #n_steps is number of steps
   
-  df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
+  # Creates a dataframe to store the random walk:
+  # x and y will store the co-ordinates of each step
+  # while t contains the time, or step number.
   
-  df[1,] <- c(0,0,1)
+  df = data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
+  
+  # Sets the first point of the walk to the origin.
+  
+  df[1, ] = c(0, 0, 1)
+  
+  # Loop that creates the walk, starting at step 2 as step 1 is the origin.
   
   for (i in 2:n_steps) {
-    
-    h <- 0.25
-    
-    angle <- runif(1, min = 0, max = 2*pi)
-    
-    df[i,1] <- df[i-1,1] + cos(angle)*h
-    
-    df[i,2] <- df[i-1,2] + sin(angle)*h
-    
-    df[i,3] <- i
-    
+    h = 0.25 # Defines the length of a run
+    angle = runif(1, min = 0, max = 2 * pi) # Generates a random angle in radians
+    df[i, 1] = df[i - 1, 1] + cos(angle) * h # Works out new x value
+    df[i, 2] = df[i - 1, 2] + sin(angle) * h # Works out new y value
   }
   
   return(df)
-  
 }
 
-data1 <- random_walk(500)
+# Plots two identical random walks
 
-plot1 <- ggplot(aes(x = x, y = y), data = data1) +
-  
+data1 = random_walk(500)
+
+plot1 = ggplot(aes(x = x, y = y), data = data1) +
   geom_path(aes(colour = time)) +
-  
   theme_bw() +
-  
   xlab("x-coordinate") +
-  
   ylab("y-coordinate")
 
-data2 <- random_walk(500)
+data2 = random_walk(500)
 
-plot2 <- ggplot(aes(x = x, y = y), data = data2) +
-  
+plot2 = ggplot(aes(x = x, y = y), data = data2) +
   geom_path(aes(colour = time)) +
-  
   theme_bw() +
-  
   xlab("x-coordinate") +
-  
   ylab("y-coordinate")
 
-grid.arrange(plot1, plot2, ncol=2)
+grid.arrange(plot1, plot2, ncol = 2)
